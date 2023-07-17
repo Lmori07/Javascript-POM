090e8f36b62c146191560cda521d5b3ffe0bf40f
Jose Osv. Morillo Villar <joseomv07@gmail.com>
2023-07-17 11:12:08 -0400

    Javascript-POM

---
+++ test/homepage-test.js
@@ -0,0 +1,20 @@
+const homepage = require('../pages/homepage');
+
+describe('Describe', function(){
+    this.timeout(50000);
+   
+    beforeEach(function(){
+     
+    });
+
+    it('POM Test Check', async function(){
+        var baseurl = 'https://www.google.com/';
+        await homepage.enter_url(baseurl);
+        await homepage.enter_search('Browserstack selenium javascript pom');
+    })
+
+    afterEach(async function(){
+        await homepage.closeBrowser();
+    });
+
+})
